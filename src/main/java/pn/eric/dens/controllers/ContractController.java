5717408e6ffcba6b5e175f029154e80a0bb4c013
duwupeng <865361183@qq.com>
2017-03-11 15:35:35 +0800

    contract list

--- src/main/java/pn/eric/dens/controllers/ContractController.java
+++ src/main/java/pn/eric/dens/controllers/ContractController.java
@@ -14,7 +14,11 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class ContractController {
@@ -22,10 +26,43 @@ public class ContractController {
     @Value("${file.attachmentPath}")
     String attachmentPath;
 
-    @RequestMapping("contract/{category}")
-    public List<String> showProduct(@PathVariable String category){
+    static Map<Integer, String> fileMaps = new HashMap<>();
+    static {
+        fileMaps.put(1,"买卖合同");
+        fileMaps.put(2,"供给能源合同");
+        fileMaps.put(3,"借款合同");
+        fileMaps.put(4,"担保合同");
+        fileMaps.put(5,"租赁与融资租赁");
+        fileMaps.put(6,"加工承揽合同");
+        fileMaps.put(7,"建设工程合同");
+        fileMaps.put(8,"运输合同");
+        fileMaps.put(9,"技术合同");
+        fileMaps.put(10,"委托行纪居间合同");
+        fileMaps.put(11,"涉外合同");
+        fileMaps.put(12,"保险合同");
+        fileMaps.put(13,"银行业务合同");
+        fileMaps.put(14,"信托合同");
+        fileMaps.put(15,"证券期货合同");
+        fileMaps.put(16,"网络信息技术合同");
+        fileMaps.put(17,"知识产权合同");
+        fileMaps.put(18,"教育文化合同");
+        fileMaps.put(19,"娱乐体育合同");
+        fileMaps.put(20,"海事海商合同");
+        fileMaps.put(21,"服务合同");
+        fileMaps.put(22,"企业组织经营合同");
+        fileMaps.put(23,"劳动关系合同");
+        fileMaps.put(24,"身份关系协议");
+        fileMaps.put(25,"其它协议");
+    }
 
-        return null;
+    @RequestMapping(value = "/contract/{category}",method = RequestMethod.GET)
+    public String showProduct(Model model,@PathVariable Integer category){
+        String dirName = fileMaps.get(category);
+        File dir  = new File(attachmentPath+File.separator+"sdl_docs"+File.separator+dirName);
+        File[]   files = dir.listFiles();
+        List<String> fileNames = Arrays.stream(files).map(file -> file.getName()).collect(Collectors.toList());
+        model.addAttribute("contracts", fileNames);
+        return "contracts";
     }
     /**
      * 附件下载
